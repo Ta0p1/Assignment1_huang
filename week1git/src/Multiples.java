434243fea8be552f8a6446438e1221b0d98943bf
Allen Huang <allenhuang041203@gmail.com>
2024-09-13 03:17:39 -0400

    Add first part

--- week1git/src/Multiples.java
+++ week1git/src/Multiples.java
@@ -1,24 +1,21 @@
 public class Multiples {
-    public static int multiples(String[] args) {
-        int count = 0;
-        for (int i = 1; i <= 1000; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                count++;
-            }
-        }
-        return count;
-    }
-    public static void main(String[] args) {
+    public static int multiples(int n, int a, int b) {
         int count = 0;
         for (int i = 1; i <= n; i++) {
             if (i % a == 0 && i % b == 0) {
                 count++;
             }
         }
+        return count;
+    }
+
+    public static int main(String[] args) {
+
         int n = Integer.parseInt(args[0]);
         int a = Integer.parseInt(args[1]);
         int b = Integer.parseInt(args[2]);
-        int result = count;
-        System.out.println(result);
+
+        return multiples(n, a, b);
     }
 }
+
